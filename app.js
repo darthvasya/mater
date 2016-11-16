d5b33eddc9461c8a663d22dd20e847cc688ccda6
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-11-16 16:57:08 +0300

    yeah! sidenaw is correct ^)

--- app.js
+++ app.js
@@ -3,19 +3,60 @@
   var app = angular.module('myApp', ['ngMaterial']);
 
 //
-  app.controller('appController', function($scope, $timeout, $mdSidenav) {
+  app.controller('appController', function($scope, $timeout, $mdSidenav, $log) {
 
-    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleLeft = buildDelayedToggler('left');
     $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function(){
+      return $mdSidenav('right').isOpen();
+    };
 
-    function buildToggler(componentId) {
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+
+    function buildToggler(navID) {
       return function() {
-        console.log(componentId);
-        $mdSidenav(componentId).toggle();
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
       }
     }
 
+    $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('right').close()
+        .then(function () {
+          $log.debug("close RIGHT is done");
+        });
+    };
   });
 //
+
+
 
 })();
