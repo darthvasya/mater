7cbdae4f7ddca127b049e6ab48578f66e2c71351
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-11-16 13:46:21 +0300

    left sidebar

---
+++ material/app.js
@@ -0,0 +1,50 @@
+(function () {
+  'use strict';
+  var app = angular.module('myApp', ['ngMaterial']);
+
+  app.controller('appController', function($scope, $mdDialog) {
+    $scope.currentNavItem = 'page1';
+
+    var imagePath = 'images/man.svg';
+
+    $scope.todos = [
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+    ];
+
+  });
+
+})();
