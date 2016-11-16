3e8d9def77dda02cbbe8eff0bde300d1248cd956
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-11-16 16:45:30 +0300

    add sideway

--- app.js
+++ app.js
@@ -2,49 +2,20 @@
   'use strict';
   var app = angular.module('myApp', ['ngMaterial']);
 
-  app.controller('appController', function($scope, $mdDialog) {
-    $scope.currentNavItem = 'page1';
+//
+  app.controller('appController', function($scope, $timeout, $mdSidenav) {
 
-    var imagePath = 'images/man.svg';
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
 
-    $scope.todos = [
-      {
-        face : imagePath,
-        what: 'Brunch this weekend?',
-        who: 'Min Li Chan',
-        when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
-      },
-      {
-        face : imagePath,
-        what: 'Brunch this weekend?',
-        who: 'Min Li Chan',
-        when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
-      },
-      {
-        face : imagePath,
-        what: 'Brunch this weekend?',
-        who: 'Min Li Chan',
-        when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
-      },
-      {
-        face : imagePath,
-        what: 'Brunch this weekend?',
-        who: 'Min Li Chan',
-        when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
-      },
-      {
-        face : imagePath,
-        what: 'Brunch this weekend?',
-        who: 'Min Li Chan',
-        when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
-      },
-    ];
+    function buildToggler(componentId) {
+      return function() {
+        console.log(componentId);
+        $mdSidenav(componentId).toggle();
+      }
+    }
 
   });
+//
 
 })();
