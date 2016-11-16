4c32fb67d0cb52a750b1b1b7b567a7c14c19fadd
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-11-16 22:51:01 +0300

    add some routing

---
+++ Controllers/appController.js
@@ -0,0 +1,66 @@
+app.controller('appController', function($scope, $timeout, $mdSidenav, $log, $location) {
+
+  $scope.sidenav_type_url = "templates/";
+
+  $scope.toggleRight = buildToggler('right');
+
+  $scope.toggleSidenav = function(type) {
+    switch (type) {
+      case 'add-project':
+       $location.path("add-project")
+        break;
+      case 'add-task':
+       $location.path("add-task")
+        break;
+    }
+    $scope.toggleRight();
+  }
+
+  $scope.isOpenRight = function(){
+    return $mdSidenav('right').isOpen();
+  };
+
+  function debounce(func, wait, context) {
+    var timer;
+
+    return function debounced() {
+      var context = $scope,
+          args = Array.prototype.slice.call(arguments);
+      $timeout.cancel(timer);
+      timer = $timeout(function() {
+        timer = undefined;
+        func.apply(context, args);
+      }, wait || 10);
+    };
+  }
+
+  function buildDelayedToggler(navID) {
+    return debounce(function() {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav(navID)
+        .toggle()
+        .then(function () {
+          $log.debug("toggle " + navID + " is done");
+        });
+    }, 200);
+  }
+
+  function buildToggler(navID) {
+    return function() {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav(navID)
+        .toggle()
+        .then(function () {
+          $log.debug("toggle " + navID + " is done");
+        });
+    }
+  }
+
+  $scope.close = function () {
+    // Component lookup should always be available since we are not using `ng-if`
+    $mdSidenav('right').close()
+      .then(function () {
+        $log.debug("close RIGHT is done");
+      });
+  };
+});
--- app.js
+++ app.js
@@ -1,64 +1,13 @@
-(function () {
-  'use strict';
-  var app = angular.module('myApp', ['ngMaterial']);
-
-//
-  app.controller('appController', function($scope, $timeout, $mdSidenav, $log) {
-
-    $scope.toggleLeft = buildDelayedToggler('left');
-    $scope.toggleRight = buildToggler('right');
-
-    $scope.isOpenRight = function(){
-      return $mdSidenav('right').isOpen();
-    };
-
-    function debounce(func, wait, context) {
-      var timer;
-
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
-
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }, 200);
-    }
-
-    function buildToggler(navID) {
-      return function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }
-    }
-
-    $scope.close = function () {
-      // Component lookup should always be available since we are not using `ng-if`
-      $mdSidenav('right').close()
-        .then(function () {
-          $log.debug("close RIGHT is done");
-        });
-    };
-
-  });
-//
-
-
-
-})();
+var app = angular.module('myApp', ['ngMaterial', 'ngRoute'])
+.config(function($routeProvider){
+    $routeProvider.when('/add-project',
+    {
+        templateUrl:'templates/add-project.html',
+        controller:'appController'
+    });
+    $routeProvider.when('/add-task',
+    {
+        templateUrl:'templates/add-task.html',
+        controller:'appController'
+    });
+});
